09476fa98e878035cb88b51a65479312a1b6bfd9
Andrii Vakarev <avakarev@gmail.com>
2023-10-07 02:34:23 +0200

    server: implement status api

--- internal/web/api/routes.go
+++ internal/web/api/routes.go
@@ -5,11 +5,13 @@ import (
 	"github.com/kataras/iris/v12"
 
 	"github.com/awtrix-light/hub/internal/web/api/health"
+	"github.com/awtrix-light/hub/internal/web/api/status"
 )
 
 // Routes adds api routes to the iris app
 func Routes(app *iris.Application) {
 	app.PartyFunc("/api", func(api iris.Party) {
 		health.Routes(api)
+		status.Routes(api)
 	})
 }
