c4bef279379b9c7e5dcf01eae911eca3be5bb221
Andrii Vakarev <avakarev@gmail.com>
2024-06-17 00:36:40 +0200

    ui: cleanup css, extract reusable styles into main.css

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -29,21 +29,3 @@ export default defineComponent({
   },
 });
 </script>
-
-<style>
-.card, .dropdown-menu, .footer, .header, .sidebar {
-  box-shadow: rgba(50, 50, 93, .02) 0 2px 5px -1px, rgba(0, 0, 0, .05) 0 1px 3px -1px;
-}
-html:not([dir="rtl"]) .wrapper {
-  padding-left: var(--cui-sidebar-occupy-start, 0);
-  padding-right: var(--cui-sidebar-occupy-end, 0);
-}
-.wrapper {
-  width: 100%;
-  will-change: auto;
-  transition: padding .15s;
-}
-.footer, .header {
-  border: 0;
-}
-</style>
